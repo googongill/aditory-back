83c690016fec6e8f4138d700e3272ea35c8ce1b5
sngjuni <wbsl3312@gmail.com>
2024-04-11 10:17:26 +0900

    Initial commit

---
+++ src/main/java/com/googongill/aditory/AditoryApplication.java
@@ -0,0 +1,13 @@
+package com.googongill.aditory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AditoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AditoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/googongill/aditory/AditoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googongill.aditory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AditoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
