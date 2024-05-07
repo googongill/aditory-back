fa3f551689a6d90732baf2e4cc2d37e6f5e666c5
sngjuni <wbsl3312@gmail.com>
2024-05-07 23:15:22 +0900

    test: UserControllerTest 테스트 수정

--- src/test/java/com/googongill/aditory/controller/UserControllerTest.java
+++ src/test/java/com/googongill/aditory/controller/UserControllerTest.java
@@ -32,7 +32,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
