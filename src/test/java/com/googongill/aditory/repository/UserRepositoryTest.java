497d1bc2b48270fe1bb117b36ab6e79af739e13e
sngjuni <wbsl3312@gmail.com>
2024-05-07 23:13:21 +0900

    test: UserRepositoryTest 테스트 코드 수정

--- src/test/java/com/googongill/aditory/repository/UserRepositoryTest.java
+++ src/test/java/com/googongill/aditory/repository/UserRepositoryTest.java
@@ -1,17 +1,23 @@
 package com.googongill.aditory.repository;
 
+import com.googongill.aditory.TestUtils;
+import com.googongill.aditory.domain.Category;
 import com.googongill.aditory.domain.User;
 import com.googongill.aditory.domain.enums.Role;
 import com.googongill.aditory.domain.enums.SocialType;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.Arrays;
 import java.util.Optional;
 
+import static com.googongill.aditory.TestDataRepository.createCategory;
+import static com.googongill.aditory.TestDataRepository.createUser;
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
@@ -25,19 +31,33 @@ class UserRepositoryTest {
     private TestEntityManager testEntityManager;
 
     @Test
-    public void findByUsername_Success() {
+    public void findById_Success() throws Exception {
         // given
-        User user = new User("testUser",
-                "testPw",
-                Role.ROLE_USER, SocialType.LOCAL,
-                "테스트 유저",
-                "010-1234-5678");
+        Long userId = 1L;
+        User user = createUser();
+        TestUtils.setEntityId(userId, user);
+        Category category = createCategory();
+        user.addCategories(Arrays.asList(category));
+        userRepository.save(user);
 
         // when
-        Optional<User> findUser = userRepository.findByUsername(user.getUsername());
+        Optional<User> foundUser = userRepository.findById(userId);
 
         // then
-        assertThat(findUser).isNotEmpty();
-        assertThat(findUser.get()).isEqualTo(user);
+        assertThat(foundUser).isNotEmpty();
+        assertThat(foundUser.get().getId()).isEqualTo(userId);
+        assertThat(foundUser.get().getCategories().get(0).getCategoryName()).isEqualTo(category.getCategoryName());
+    }
+
+    @Test
+    public void findById_NotExists_ReturnsEmptyOptional() {
+        // given
+        Long nonExistentUserId = 999L; // 존재하지 않는 ID
+
+        // when
+        Optional<User> foundUser = userRepository.findById(nonExistentUserId);
+
+        // then
+        assertThat(foundUser).isEmpty();
     }
 }
