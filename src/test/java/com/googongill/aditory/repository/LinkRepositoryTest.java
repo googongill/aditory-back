2f608a5736e5ecc44ae2b232cf9419b37817e985
sngjuni <wbsl3312@gmail.com>
2024-05-08 16:12:31 +0900

    test: LinkRepositoryTest 테스트 코드 수정

--- src/test/java/com/googongill/aditory/repository/LinkRepositoryTest.java
+++ src/test/java/com/googongill/aditory/repository/LinkRepositoryTest.java
@@ -16,8 +16,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
-
 @Slf4j
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
