e929cbe03453e8017e9354bafa0545de29ceb4d2
sngjuni <wbsl3312@gmail.com>
2024-05-08 16:11:42 +0900

    test: LinkRepositoryTest 테스트 코드 추가

---
+++ src/test/java/com/googongill/aditory/repository/LinkRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.googongill.aditory.repository;
+
+import com.googongill.aditory.TestDataRepository;
+import com.googongill.aditory.domain.Category;
+import com.googongill.aditory.domain.Link;
+import com.googongill.aditory.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@Slf4j
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class LinkRepositoryTest {
+
+    @InjectMocks
+    private TestDataRepository testDataRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private LinkRepository linkRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void findTop10ByUserAndLinkStateOrderByCreatedAtAsc_Success() throws Exception {
+        // given
+        User user = testDataRepository.createUser();
+        userRepository.save(user);
+        Category category = testDataRepository.createCategory();
+        user.addCategories(Arrays.asList(category));
+        categoryRepository.save(category);
+        Link link = testDataRepository.createLink(category, user);
+        linkRepository.save(link);
+
+        List<Link> targetLinks = Arrays.asList(link);
+
+        // when
+        List<Link> actualLinks = linkRepository.findTop10ByUserAndLinkStateOrderByCreatedAtAsc(user, false);
+
+        // then
+        Assertions.assertThat(actualLinks.get(0).getTitle()).isEqualTo(targetLinks.get(0).getTitle());
+    }
+}
