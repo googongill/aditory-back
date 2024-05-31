c11af01afd6152b430aa4eaeda49e6869718aee6
sngjuni <wbsl3312@gmail.com>
2024-05-31 23:09:43 +0900

    feat: 프로필 이미지 로직 수정, 로그인 시 contact 추가

---
+++ src/main/java/com/googongill/aditory/repository/ProfileImageRepository.java
@@ -0,0 +1,7 @@
+package com.googongill.aditory.repository;
+
+import com.googongill.aditory.domain.ProfileImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
+}
