d016f52a06b3d6401898dce56a26e9c892a3e745
sngjuni <wbsl3312@gmail.com>
2024-12-16 17:17:57 +0900

    refactor: BaseEntity 코틀린으로 변경

---
+++ src/main/java/com/googongill/aditory/domain/BaseEntity.kt
@@ -0,0 +1,21 @@
+package com.googongill.aditory.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import lombok.Getter
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+open class BaseEntity : BaseTimeEntity() {
+    @CreatedBy
+    @Column(updatable = false)
+    private val createdBy: String? = null
+
+    @LastModifiedBy
+    private val lastModifiedBy: String? = null
+}
